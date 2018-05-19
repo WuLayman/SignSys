6d2dec7a34bb363a40c2952473ed45615df3808c
吴伟果 <769014190@qq.com>
2018-05-19 14:53:29 +0800

    Client

--- SignSys/Shell/Shell.xaml.cs
+++ SignSys/Shell/Shell.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PublicBaseClassProj
+namespace Shell
 {
     /// <summary>
-    /// UserControl1.xaml 的交互逻辑
+    /// Shell.xaml 的交互逻辑
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class Shell : Window
     {
-        public UserControl1()
+        public Shell()
         {
             InitializeComponent();
         }
