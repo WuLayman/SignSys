b5cb44a48e042005e2cde8eb8aa0ecb7b4727ea6
吴伟果 <769014190@qq.com>
2018-05-19 13:06:58 +0800

    初始

---
+++ SignSys/PublicBaseClass/PersonInfo.cs
@@ -0,0 +1,25 @@
+﻿using PublicBaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicBaseClassProj
+{
+    public class PersonInfo : NotifyProperyChangedBase
+    {
+        private string _userNickName;
+        public string UserNickName { get => _userNickName; set { _userNickName = value; OnProperyChanged("UserNickName"); } }
+
+        private string _userRealName;
+        public string UserRealName { get => _userRealName; set { _userRealName = value; OnProperyChanged("UserRealName"); } }
+
+        private string _passWord;
+        public string PassWord { get => _passWord; set { _passWord = value; OnProperyChanged("PassWork"); } }
+
+        private string _macAddress;
+        public string MacAddress { get => _macAddress; set { _macAddress = value; OnProperyChanged("MacAddress"); } }
+
+    }
+}
---
+++ SignSys/PublicBaseClass/PictureInfo.cs
@@ -0,0 +1,23 @@
+﻿using PublicBaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicBaseClassProj
+{
+    public class PictureInfo : NotifyProperyChangedBase
+    {
+        private string _userNickName;
+        private byte[] _picture;
+
+        private TimetableAndExpPic _ttAndEP;
+
+        public TimetableAndExpPic TtAndEP { get => _ttAndEP; set { _ttAndEP = value; OnProperyChanged("TtAndEP"); } }
+
+        public string UserNickName { get => _userNickName; set { _userNickName = value; OnProperyChanged("UserNickName"); } }
+        public byte[] Picture { get => _picture; set { _picture = value; OnProperyChanged("Picture"); } }
+
+    }
+}
