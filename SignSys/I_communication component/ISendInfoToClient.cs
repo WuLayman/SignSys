84b85f7e61120c4c8cb32efae6adf6fed98a8a86
吴伟果 <769014190@qq.com>
2018-05-20 21:30:32 +0800

    WCF

--- SignSys/I_communication component/ISendInfoToClient.cs
+++ SignSys/I_communication component/ISendInfoToClient.cs
@@ -63,11 +63,11 @@ namespace I_communication_component
         #endregion
         public void Run(CancellationToken ct)
         {
+            back = new CallBack();
+            InstanceContext context = new InstanceContext(back);
             using (DuplexChannelFactory<IService> channelFactory = new DuplexChannelFactory<IService>(context, "NetTcpBinding_IService"))
             {
-                IService proxy = channelFactory.CreateChannel();
-                back = new CallBack();
-                InstanceContext context = new InstanceContext(back);
+                IService proxy = channelFactory.CreateChannel();               
                 using (proxy as IDisposable)
                 {
                     proxy.Login(personInfo);
