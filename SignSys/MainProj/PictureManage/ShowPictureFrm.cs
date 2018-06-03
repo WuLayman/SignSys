741bf6c67112797e3d3ba406b030a5cad23fd929
ZWJ <zwj@163.com>
2018-06-03 11:01:27 +0800

    添加图片缩放移动功能

--- SignSys/MainProj/PictureManage/ShowPictureFrm.cs
+++ SignSys/MainProj/PictureManage/ShowPictureFrm.cs
@@ -14,6 +14,14 @@ namespace MainProj.PictureManage
 {
     public partial class ShowPictureFrm : Form
     {
+        private const double step = 0.1;
+        //是否正在拖拽 
+        bool isDrag = false;
+        //鼠标按下坐标（control控件的相对坐标） 
+        Point mouseDownPoint = Point.Empty;
+        //将被拖动的控件 
+        private Control control;
+
         public ShowPictureFrm()
         {
             InitializeComponent();
@@ -26,6 +34,63 @@ namespace MainProj.PictureManage
             cboChooseType.Items.Add("实验表");
             cboChooseType.SelectedIndex = 0;
             textBox1.Text = "请填写真实姓名";
+            this.picTimeTable.MouseWheel += new MouseEventHandler(PicTimeTable_MouseWheel);
+            this.picTimeTable.MouseMove += new MouseEventHandler(PicTimeTable_MouseMove);
+            this.picTimeTable.MouseDown += new MouseEventHandler(PicTimeTable_MouseDown);
+            this.picTimeTable.MouseUp += new MouseEventHandler(PicTimeTable_MouseUp);
+        }
+
+        private void PicTimeTable_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                isDrag = false;
+            }
+        }
+
+        private void PicTimeTable_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrag = true;
+            control = picTimeTable;
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint = picTimeTable.Location;
+            }
+        }
+
+        private void PicTimeTable_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                Point mousePos = new Point(control.Location.X, control.Location.Y);
+                mousePos.Offset(e.X, e.Y);
+                mousePos.X -= this.control.Width / 2;
+                mousePos.Y -= this.control.Height / 2;
+                control.Location = mousePos;
+            }
+        }
+
+
+        private void PicTimeTable_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                if (picTimeTable.Width < 12000 && picTimeTable.Height < 7000)
+                {
+                    picTimeTable.Width = picTimeTable.Width + int.Parse(Math.Ceiling(picTimeTable.Width * step).ToString());
+                    picTimeTable.Height = picTimeTable.Height + int.Parse(Math.Ceiling(picTimeTable.Height * step).ToString());
+                    picTimeTable.Location = new Point((groupBox1.Width - picTimeTable.Width) / 2, (groupBox1.Height - picTimeTable.Height) / 2);
+                }
+            }
+            else
+            {
+                if (picTimeTable.Width > 70 && picTimeTable.Height > 40)
+                {
+                    picTimeTable.Width = picTimeTable.Width - int.Parse(Math.Ceiling(picTimeTable.Width * step).ToString());
+                    picTimeTable.Height = picTimeTable.Height - int.Parse(Math.Ceiling(picTimeTable.Height * step).ToString());
+                    picTimeTable.Location = new Point((groupBox1.Width - picTimeTable.Width) / 2, (groupBox1.Height - picTimeTable.Height) / 2);
+                }
+            }
         }
 
         private void btnQuery_Click(object sender, EventArgs e)
